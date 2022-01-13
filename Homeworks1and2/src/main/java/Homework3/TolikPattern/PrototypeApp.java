e075f088aa6c7e9bd637dcf621094f4f4da9792e
EpifanovTolik <92646791+EpifanovTolik@users.noreply.github.com>
2022-01-13 23:30:22 +0300

    Добавил свои паттерны

---
+++ Homeworks1and2/src/main/java/Homework3/TolikPattern/PrototypeApp.java
@@ -0,0 +1,42 @@
+package Homework3.TolikPattern;
+
+
+public class PrototypeApp {
+    public static void main(String[] args) {
+        Human original = new Human(20, "Petya");
+        System.out.println(original);
+
+        Human notOrignal =(Human)original.copy();
+        System.out.println(notOrignal);
+
+        Human notOriginal2 =(Human)original.copy();
+        System.out.println(notOriginal2);
+
+    }
+}
+
+interface Copyable {
+    Object copy();
+}
+class Human implements Copyable /*Cloneable*/{
+    int age;
+    String name;
+    public Human(int age, String name){
+        this.age=age;
+        this.name=name;
+    }
+
+    @Override
+    public String toString() {
+        return "Human{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+    public Object copy(){
+        Human clonn = new Human(age,name);
+        return clonn;
+    }
+}
+
+
---
+++ Homeworks1and2/src/main/java/Homework3/TolikPattern/ProxyApp.java
@@ -0,0 +1,43 @@
+package Homework3.TolikPattern;
+
+
+public class ProxyApp {
+    public static void main(String[] args) {
+
+        Image image = new ProxyImage("D:/image/my.jpg");
+        image.display();
+    }
+}
+interface Image{
+    void display();
+}
+
+class RealImage implements Image{
+    String file;
+    public RealImage(String file){
+        this.file=file;
+        load();
+    }
+    void load(){
+        System.out.println("загрузка "+file);
+    }
+
+    public void display(){
+        System.out.println("просмотр "+file);
+    }
+}
+class ProxyImage implements Image{
+
+    String file;
+    RealImage image;
+    public ProxyImage(String file){
+        this.file=file;
+    }
+
+    public void display(){
+        if(image==null){
+            image = new RealImage(file);
+        }
+        image.display();
+    }
+}
